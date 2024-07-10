a5b6d8de0106f3bf56c67f34d7be1d95a3abdbf8
VEDANT NIGAM <vedantnigam0104@gmail.com>
2024-07-10 20:42:40 +0530

    Update API endpoint URL in CreateListing.jsx

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -133,7 +133,7 @@ export default function CreateListing() {
         return setError('Discount price must be lower than regular price');
       setLoading(true);
       setError(false);
-      const res = await fetch('https://heaven-hills-frontend.onrender.com/api/listing/create', {
+      const res = await fetch('https://heaven-hills.onrender.com/api/listing/create', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
