ba1e09ab5ed4eedffab2c930fd1efae629dc6f2d
VEDANT NIGAM <vedantnigam0104@gmail.com>
2024-07-10 16:40:58 +0530

    dfghj

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -17,7 +17,7 @@ export default function SignUp() {
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await fetch('https://heaven-hills-1.onrender.com/api/auth/signup', {
+      const res = await fetch('https://heaven-hills.onrender.com/api/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
