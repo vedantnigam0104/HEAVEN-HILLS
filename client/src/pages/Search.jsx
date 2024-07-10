7c54a81f765bff522d8a617d6aed0c4092ed2cec
VEDANT NIGAM <vedantnigam0104@gmail.com>
2024-07-10 18:58:54 +0530

    final

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -120,7 +120,7 @@ export default function Search() {
     const urlParams = new URLSearchParams(location.search);
     urlParams.set('startIndex', startIndex);
     const searchQuery = urlParams.toString();
-    const res = await fetch(`https://heaven-hills/api/listing/get?${searchQuery}`);
+    const res = await fetch(`https://heaven-hills.onrender.com/api/listing/get?${searchQuery}`);
     const data = await res.json();
     if (data.length < 9) {
       setShowMore(false);
