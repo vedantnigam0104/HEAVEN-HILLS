42ed234f9fe935d690eab494f08a81fe8c818ee0
VEDANT NIGAM <vedantnigam0104@gmail.com>
2024-07-10 14:12:28 +0530

    first commit

--- api/index.js
+++ api/index.js
@@ -5,7 +5,8 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
-import path from 'path';
+import cors from 'cors';
+//import path from 'path';
 dotenv.config();
 
 mongoose
@@ -17,9 +18,16 @@ mongoose
     console.log(err);
   });
 
-  const __dirname = path.resolve();
+  //const __dirname = path.resolve();
 
 const app = express();
+
+// Example: Allow requests from http://localhost:5173
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
+
 
 app.use(express.json());
 
@@ -34,19 +42,18 @@ app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+//app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
-
+///app.get('*', (req, res) => {
+  //res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+//})
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-      success: false,
-      statusCode,
-      message,
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
   });
+});
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    port: 5173, // Set the port for the frontend server
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
+        changeOrigin: true,
+        //rewrite: (path) => path.replace(/^\/api/, ''),
         secure: false,
       },
     },
   },
   plugins: [react()],
-})
+});
+
+
