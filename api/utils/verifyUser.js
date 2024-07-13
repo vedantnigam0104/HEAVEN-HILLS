4efffcfb7b865f993f6721aa96dbd581bdc67af5
VEDANT NIGAM <vedantnigam0104@gmail.com>
2024-07-13 16:26:59 +0530

    iuhiuiu

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,16 +1,42 @@
-import jwt from 'jsonwebtoken';
-import { errorHandler } from './error.js';
+// import jwt from 'jsonwebtoken';
+// import { errorHandler } from './error.js';
 
-export const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
-  console.log(token ,"--> In backend")
+// export const verifyToken = (req, res, next) => {
+//   const token = req.cookies.access_token;
+//   console.log(token ,"--> In backend")
 
-  if (!token) return next(errorHandler(401, 'Unauthorized'));
+//   if (!token) return next(errorHandler(401, 'Unauthorized'));
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return next(errorHandler(403, 'Forbidden'));
+//   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+//     if (err) return next(errorHandler(403, 'Forbidden'));
 
-    req.user = user;
-    next();
-  });
+//     req.user = user;
+//     next();
+//   });
+// };
+
+
+const jwt = require('jsonwebtoken');
+
+const verifyUser = (req, res, next) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+
+    console.log("In validateUser");
+
+    if (token === null || token === undefined) { // Check explicitly for null or undefined
+        return res.status(401).json({ message: 'Unauthorized user. No token provided.' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log("Decoded Token:", decoded); // Log the decoded token
+        req.user = decoded.user;
+        next();
+    } catch (error) {
+        console.error("Error decoding token:", error);
+        res.status(400).json({ message: 'Unauthorized user. Invalid token.' });
+    }
 };
+
+
+module.exports = verifyUser;
